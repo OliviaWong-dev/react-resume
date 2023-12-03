9e3960825a1abbf6aaa53b66082bdb5e7014cad8
Olivia <olivia.wy.wong@gmail.com>
2023-12-03 00:19:53 -0800

    Revert "test"
    This reverts commit cda0f092232b6fbeeeb17bb4afad30714aafa655.

--- src/components/Layout/Section.tsx
+++ src/components/Layout/Section.tsx
@@ -9,7 +9,6 @@ const Section: FC<
   return (
     <section className={classNames(className, {'px-4 py-16 md:py-24 lg:px-8': !noPadding})} id={sectionId}>
       <div className={classNames({'mx-auto max-w-screen-lg': !noPadding})}>{children}</div>
-      test
     </section>
   );
 });
